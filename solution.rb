7b3771debd914e5820eef0f134814550a5123253
Kendall Carey <kcarey@dayspring-tech.com>
2017-02-07 20:20:57 -0800

    created a new class that would load the usage csv and wrote test

--- solution.rb
+++ solution.rb
@@ -1,5 +1,6 @@
+require 'csv'
+
 class Question
-  require 'csv'
 
   attr_accessor :strand_id,
                 :strand_name,
@@ -25,3 +26,25 @@ class Question
     @difficulty = attrs[:difficulty]
   end
 end
+
+class Usage
+  attr_accessor :student_id,
+                :question_id,
+                :assigned_hours_ago,
+                :answered_hours_ago
+
+  def self.load_from_csv
+    csv = CSV.read('usage.csv', headers:true)
+    csv.each do |row|
+      puts row.to_hash
+      self.new(row.to_hash)
+    end
+  end
+    def initialize(attrs = {})
+    @student_id = attrs[:student_id]
+    @question_id = attrs[:question_id]
+    @assigned_hours_ago = attrs[:assigned_hours_ago]
+    @answered_hours_ago = attrs[:answered_hours_ago]
+  end
+end
+
--- solution_spec.rb
+++ solution_spec.rb
@@ -9,4 +9,9 @@ describe Question do
 
 end
 
-
+describe Usage  do
+  it 'should load the usage csv' do
+    usage = Usage.load_from_csv
+    expect(usage.length > 0).to be true
+  end
+end
